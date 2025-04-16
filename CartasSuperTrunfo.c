8b704b6b004ab2b153ea7be900be62dd73e7fbc3
Dcardoso23 <140876098+Dcardoso23@users.noreply.github.com>
2025-04-16 04:21:22 +0000

    att2 finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -53,5 +53,52 @@ int main() {
    printf("Digite Numeros de Pontos Turisticos: ");
    scanf("%d", &pontosTuristicos);
 
-    return 0;
+   getchar(); // limpa o buffer antes da proxima leitura com fgets é a mesma coisa que voce apagar um caractere no final de um texto tipo um ~ que faria o leitor interpretar errado
+
+   printf("\n Cadastro de Carta 2:\n");
+
+   printf("Digite um Estado (ex: Rio de Janeiro): ");
+   fgets(estado2, sizeof(estado2), stdin); // sizeof define o tamanho maximo a ser lido
+   estado2[strcspn(estado2, "\n")] = '\0';
+
+   printf("Digite o Codigo da Carta (ex: B1): ");
+   fgets(codigo2, sizeof(codigo2), stdin); // sizeof define o tamanho maximo a ser lido
+   codigo2[strcspn(codigo2, "\n")] = '\0';
+
+   printf("Digite o Nome da Cidade (ex: São Francisco de Itabapoana): ");
+   fgets(nomeCidade2, sizeof(nomeCidade2), stdin); // sizeof define o tamanho maximo a ser lido
+   nomeCidade2[strcspn(nomeCidade2, "\n")] = '\0';
+
+   printf("Digite a População: ");
+   scanf("%d", &populacao2);
+
+   printf("Area: (em km2): ");
+   scanf("%f", &area2);
+
+   printf("PIB: (em bilhoes) ");
+   scanf("%f", &pib2);
+
+   printf("Digite o Numero de Pontos Turisticos: ");
+   scanf("%d", &pontosTuristicos2);
+
+   printf("\n--- Dados das Cartas ---\n\n");
+   printf("Carta 1:\n");
+   printf("Estado: %s\n", estado);
+   printf("codigo: %s\n", codigo);
+   printf("Nome da Cidade: %s\n", nomeCidade);
+   printf("População: %d\n", populacao);
+   printf("Area: %.2f km2\n", area); // %.2f é um especificador de formato utilizado para formatar e exibir  o valor como um numero de ponto flutuante co duas casas decimais
+   printf("PIB %.2f Bilhões de reais\n", pib);
+   printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos);
+
+   printf("\nCarta 2:\n");
+   printf("Estado: %s\n", estado2);
+   printf("codigo: %s\n", codigo2);
+   printf("Nome da Cidade: %s\n", nomeCidade2);
+   printf("População: %d\n", populacao2);
+   printf("Area: %.2f km2\n", area2); // %.2f é um especificador de formato utilizado para formatar e exibir  o valor como um numero de ponto flutuante co duas casas decimais
+   printf("PIB %.2f Bilhões de reais\n", pib2);
+   printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+
+   return 0;
 }
